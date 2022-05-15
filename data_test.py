aeb9045d62fd882cd542925f097222a20c5b9de5
aviv21 <avivishs@gmail.com>
2022-05-15 19:32:15 +0300

    first_working_model_80_semples

--- data_test.py
+++ data_test.py
@@ -33,20 +33,20 @@ def make_model(input_shape):
     num_classes  = 4
     input_layer = keras.layers.Input(input_shape)
 
-    conv1 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(input_layer)
+    conv1 = keras.layers.Conv1D(filters=128, kernel_size=3, padding="same")(input_layer)
     conv1 = keras.layers.BatchNormalization()(conv1)
     conv1 = keras.layers.ReLU()(conv1)
-    conv1 = keras.layers.MaxPooling1D(6,padding='same')(conv1)
+    conv1 = keras.layers.MaxPooling1D(2,padding='same')(conv1)
 
-    conv2 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(conv1)
+    conv2 = keras.layers.Conv1D(filters=128, kernel_size=3, padding="same")(conv1)
     conv2 = keras.layers.BatchNormalization()(conv2)
     conv2 = keras.layers.ReLU()(conv2)
-    conv2 = keras.layers.MaxPooling1D(4,padding='same')(conv2)
+    conv2 = keras.layers.MaxPooling1D(6,padding='same')(conv2)
 
-    conv3 = keras.layers.Conv1D(filters=64, kernel_size=3, padding="same")(conv2)
+    conv3 = keras.layers.Conv1D(filters=128, kernel_size=3, padding="same")(conv2)
     conv3 = keras.layers.BatchNormalization()(conv3)
     conv3 = keras.layers.ReLU()(conv3)
-    conv3 = keras.layers.MaxPooling1D(3,padding='same')(conv3)
+    conv3 = keras.layers.MaxPooling1D(6,padding='same')(conv3)
 
     gap = keras.layers.GlobalAveragePooling1D()(conv3)
 
@@ -58,17 +58,61 @@ def make_model(input_shape):
 
 def main():
     x_train = []
-    for i in range(1, 21):
-        path = './signals/shhs1-2000' + str(i).zfill(2) + '.edf'
-        x_train.append(edf_get_oximetry(path)[0:21600])
-    #x_train = edf_get_oximetry('./signals/shhs1-200001.edf')
-    x_train = np.stack(x_train, axis=0)
-    x_train = x_train.reshape(-1,20,21600)
-    print(x_train.shape)
-    y_train = get_labels('./shhs1-dataset-0.14.0.csv')[0:20]
+    semple_length=21600
+    num_of_semples=80
+    y_train = get_labels('./shhs1-dataset-0.14.0.csv')[0:num_of_semples]
     y_train = np.array(y_train)
-    y_train = y_train.reshape(-1,20)
-    print(y_train.shape)
-    model = make_model(input_shape=(1,21600))
+    y_train = y_train.reshape(num_of_semples,-1)
+    for i in range(1, num_of_semples+1):
+        path = './signals/shhs1-2000' + str(i).zfill(2) + '.edf'
+        temp=edf_get_oximetry(path)[0:semple_length]
+        if np.shape(temp) == (semple_length,):
+            x_train.append(temp)
+        else:
+            y_train = np.delete(y_train, i-1, 0)
+    x_train = np.stack(x_train, axis=0)
+    x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
+    model = make_model(input_shape=x_train.shape[1:])
     keras.utils.plot_model(model, show_shapes=True)
+
+    epochs = 10
+    batch_size = 8
+
+    callbacks = [
+        keras.callbacks.ModelCheckpoint(
+            "best_model.h5", save_best_only=True, monitor="val_loss"
+        ),
+        keras.callbacks.ReduceLROnPlateau(
+            monitor="val_loss", factor=0.5, patience=20, min_lr=0.0001
+        ),
+        keras.callbacks.EarlyStopping(monitor="val_loss", patience=50, verbose=1),
+    ]
+    model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["sparse_categorical_accuracy"],
+    )
+    history = model.fit(
+        x_train,
+        y_train,
+        batch_size=batch_size,
+        epochs=epochs,
+        callbacks=callbacks,
+        validation_split=0.2,
+        verbose=1,
+    )
+
+    metric = "sparse_categorical_accuracy"
+    plt.figure()
+    plt.plot(history.history[metric])
+    plt.plot(history.history["val_" + metric])
+    plt.title("model " + metric)
+    plt.ylabel(metric, fontsize="large")
+    plt.xlabel("epoch", fontsize="large")
+    plt.legend(["train", "val"], loc="best")
+    plt.show()
+    plt.close()
+
+
+
 main()
