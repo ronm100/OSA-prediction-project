3b6204849127cb7e18dcdd7974bbbb02389ea4bf
ronm100 <57960026+ronm100@users.noreply.github.com>
2022-09-17 19:25:20 +0300

    basic categorization framework implemented

--- data_categorization.py
+++ data_categorization.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import numpy as np
-# from .edf_pre_proccecing import ahi_to_label
 import matplotlib.pyplot as plt
 
 CAT_DIR = './shhs1-dataset-0.14.0.csv'
-# CATEGORIES = ['race', 'age_category_s1', 'gender', 'bmi_s1', 'EVSMOK15']
-CATEGORIES = ['race', 'gender']
+CATEGORIES = ['race', 'gender', 'age_category_s1', 'smokstat_s1', 'bmi']
 sub_categories = {
     'race': ['white', 'black', 'other'],
-    'gender': ['male', 'female']
+    'gender': ['male', 'female'],
+    'age_category_s1': ['35-44', '45-54', '55-64', '65-74', '75-84', '85 or older'],
+    'smokstat_s1': ['Never', 'Current', 'Former'],
+    'bmi': ['18-20', '20-30', '30-40', '40-50']
 }
 RAW_LABEL_COL = 'ahi_a0h3a'
 
@@ -20,6 +21,7 @@ def count_occurances(df, cat_col_name):
         occ[val] = occ.setdefault(val, 0) + 1
     return occ
 
+
 def ahi_to_label(ahi):
     if ahi < 5:
         return 0
@@ -30,14 +32,16 @@ def ahi_to_label(ahi):
     else:
         return 3
 
+
 def count_matching_cells(df, category, sub_category, label):
     cat_df = df[df['label'] == label]
     cat_df = cat_df[cat_df[category] == sub_category]
     return len(cat_df)
 
+
 def get_label_distr(df, category):
     no, mild, moderate, severe = [], [], [], []
-    for sub_category in df[category].unique():
+    for sub_category in df[category].dropna().unique():
         no.append(count_matching_cells(df, category, sub_category, label=0))
         mild.append(count_matching_cells(df, category, sub_category, label=1))
         moderate.append(count_matching_cells(df, category, sub_category, label=2))
@@ -48,13 +52,14 @@ def get_label_distr(df, category):
 def plot_categories():
     df = pd.read_csv(CAT_DIR)
     df['label'] = df.apply(lambda x: ahi_to_label(x[RAW_LABEL_COL]), axis=1)
+    df['bmi'] = df.apply(lambda x: 10 * np.round(x['bmi_s1'] / 10), axis=1).dropna()
     for category in CATEGORIES:
-        x_axis = np.arange(len(df[category].unique()))
+        x_axis = np.arange(len(df[category].dropna().unique()))
         no, mild, moderate, severe = get_label_distr(df, category)
         plt.bar(x_axis, no, 0.1, label='Healthy')
-        plt.bar(x_axis+.1, mild, 0.1, label='Mild')
-        plt.bar(x_axis+.2, moderate, 0.1, label='Moderate')
-        plt.bar(x_axis+.3, severe, 0.1, label='Severe')
+        plt.bar(x_axis + .1, mild, 0.1, label='Mild')
+        plt.bar(x_axis + .2, moderate, 0.1, label='Moderate')
+        plt.bar(x_axis + .3, severe, 0.1, label='Severe')
         plt.xticks(x_axis, sub_categories[category])
         plt.xlabel(category)
         plt.ylabel("Number of Subjects")
