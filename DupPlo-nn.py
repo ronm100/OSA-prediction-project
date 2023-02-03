164c33680cdf49261df7b3baecf4a3ebfae0cc4c
ronm100 <ronm64@gmail.com>
2023-02-03 11:16:19 +0000

    architecture improvements

--- DupPlo-nn.py
+++ DupPlo-nn.py
@@ -17,19 +17,20 @@ LOG_BASE_DIR = ROOT.joinpath('databases/ronmaishlos@staff.technion.ac.il/logs')
 CSV_DIR = ROOT.joinpath('databases/ronmaishlos@staff.technion.ac.il/processed_data_as_csv')
 STFT_DIR = CSV_DIR.joinpath('stft')
 
-def conv2d_block(input_layer, filters, kernel_size, pooling='max', padding='valid'):
-    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation='relu', padding=padding)(input_layer)
-    conv = keras.layers.BatchNormalization()(conv)
-    conv = keras.layers.AveragePooling2D()(conv) if pooling == 'avg' else keras.layers.MaxPooling2D()(conv)
-    conv = keras.layers.Dropout(0.2)(conv)
-    return conv
-
-def conv1d_block(input_layer, filters, kernel_size, pooling='max', padding='same'):
-    conv = keras.layers.Conv1D(filters=filters, kernel_size=kernel_size, padding=padding, dilation_rate=2)(input_layer)
+def conv2d_block(input_layer, filters, kernel_size=3, dropout=0.25, pooling='max', pool_size=(1,2)):
+    conv = keras.layers.Conv2D(filters=filters, kernel_size=kernel_size, activation='relu', padding='same')(input_layer)
     conv = keras.layers.BatchNormalization()(conv)
     conv = keras.layers.ReLU()(conv)
-    conv = keras.layers.AveragePooling1D(2, padding='same')(conv) if pooling == 'avg' else keras.layers.MaxPooling1D(2, padding='same')(conv)
-    conv = keras.layers.Dropout(0.2)(conv)
+    conv = keras.layers.AveragePooling2D(pool_size)(conv) if pooling == 'avg' else keras.layers.MaxPooling2D(pool_size)(conv)
+    conv = keras.layers.Dropout(dropout)(conv)
+    return conv
+
+def conv1d_block(input_layer, filters, kernel_size=3, dropout=0.25, pooling='max', pool_size=2):
+    conv = keras.layers.Conv1D(filters=filters, kernel_size=kernel_size, padding='same', dilation_rate=2)(input_layer)
+    conv = keras.layers.BatchNormalization()(conv)
+    conv = keras.layers.ReLU()(conv)
+    conv = keras.layers.AveragePooling1D(pool_size, padding='same')(conv) if pooling == 'avg' else keras.layers.MaxPooling1D(pool_size, padding='same')(conv)
+    conv = keras.layers.Dropout(dropout)(conv)
     return conv
 
 def make_model(orig_input_shape, stft_input_shape):
@@ -38,38 +39,39 @@ def make_model(orig_input_shape, stft_input_shape):
     stft_input_layer = keras.layers.Input(stft_input_shape)
 
     # STFT - CNN:
-    conv2d_1 = conv2d_block(filters=4, kernel_size=3, pooling='avg', padding='same', input_layer=stft_input_layer)
-    conv2d_2 = conv2d_block(filters=8, kernel_size=3, padding='same', input_layer=conv2d_1)
-    conv2d_3 = conv2d_block(filters=16, kernel_size=(4, 2), input_layer=conv2d_2)
-    conv2d_4 = conv2d_block(filters=32, kernel_size=(4, 2), input_layer=conv2d_3)
-    # conv_5 = conv2d_block(filters=64, kernel_size=(3, 2), input_layer=conv_4)
-    # conv_6 = conv2d_block(filters=128, kernel_size=(3, 2), input_layer=conv_5)
-    # conv_7 = conv2d_block(filters=256, kernel_size=(3, 2), input_layer=conv_6)
-
-    stft_gap = keras.layers.GlobalAveragePooling2D()(conv2d_3)
+    conv2d_1 = conv2d_block(filters=4, kernel_size=(3,9), dropout=0.1, pooling='avg', input_layer=stft_input_layer)
+    conv2d_2 = conv2d_block(filters=8, kernel_size=(3,9), input_layer=conv2d_1)
+    conv2d_3 = conv2d_block(filters=16, kernel_size=(3,5), input_layer=conv2d_2)
+    conv2d_4 = conv2d_block(filters=32, pool_size=2, input_layer=conv2d_3)
+    conv2d_5 = conv2d_block(filters=64, pool_size=2, input_layer=conv2d_4)
+    conv2d_6 = conv2d_block(filters=128, pool_size=2, input_layer=conv2d_5)
+    conv2d_7 = conv2d_block(filters=256, pool_size=2, input_layer=conv2d_6)
+    conv2d_8 = conv2d_block(filters=256, pool_size=2, input_layer=conv2d_7)
+    stft_flat = keras.layers.Flatten()(conv2d_8)
+    stft_flat = keras.layers.Dense(128, activation="softmax")(stft_flat)
 
     # Duplo - RNN:
-    conv_lstm1 = conv1d_block(orig_input_layer, filters=128, kernel_size=3, pooling='avg')
-    conv_lstm2 = conv1d_block(conv_lstm1, filters=128, kernel_size=3)
-    lstm_1 = keras.layers.LSTM(128)(conv_lstm2)
-    lstm_1 = keras.layers.Reshape((128, -1))(lstm_1)
-    conv_lstm3 = conv1d_block(lstm_1, filters=64, kernel_size=3, pooling='avg')
-    conv_lstm4 = conv1d_block(conv_lstm3, filters=64, kernel_size=3)
+    conv_lstm1 = conv1d_block(orig_input_layer, filters=32, kernel_size=9, dropout=0.1, pooling='avg')
+    conv_lstm2 = conv1d_block(conv_lstm1, filters=64, kernel_size=3)
+    lstm_1 = keras.layers.LSTM(64)(conv_lstm2)
+    lstm_1 = keras.layers.Reshape((64, -1))(lstm_1)
+    conv_lstm3 = conv1d_block(lstm_1, filters=128, kernel_size=3, pooling='avg')
+    conv_lstm4 = conv1d_block(conv_lstm3, filters=128, kernel_size=3)
     lstm_2 = keras.layers.LSTM(128)(conv_lstm4)
 
     # Duplo CNN:
-    conv1 = conv1d_block(orig_input_layer, filters=128, kernel_size=3, pooling='avg', padding='causal')
-    conv2 = conv1d_block(conv1, filters=128, kernel_size=3)
-    conv3 = conv1d_block(conv2, filters=128, kernel_size=3)
-    conv4 = conv1d_block(conv3, filters=128, kernel_size=3)
-    conv5 = conv1d_block(conv4, filters=64, kernel_size=3)
+    conv1 = conv1d_block(orig_input_layer, filters=16, kernel_size=9, dropout=0.1, pooling='avg', pool_size=4)
+    conv2 = conv1d_block(conv1, filters=16, kernel_size=3, pool_size=4)
+    conv3 = conv1d_block(conv2, filters=32, kernel_size=3, pool_size=4)
+    conv4 = conv1d_block(conv3, filters=32, kernel_size=3)
+    conv5 = conv1d_block(conv4, filters=64, kernel_size=3, pool_size=4)
     conv6 = conv1d_block(conv5, filters=64, kernel_size=3)
-    conv7 = conv1d_block(conv6, filters=64, kernel_size=3)
-    conv8 = conv1d_block(conv7, filters=64, kernel_size=3)
+    conv7 = conv1d_block(conv6, filters=128, kernel_size=3)
+    conv8 = conv1d_block(conv7, filters=256, kernel_size=3)
+    cnn_flat = keras.layers.Flatten()(conv8)
+    cnn_flat = keras.layers.Dense(128, activation="softmax")(cnn_flat)
 
-    duplo_gap = keras.layers.GlobalAveragePooling1D()(conv8)
-
-    concatted = keras.layers.Concatenate()([duplo_gap, stft_gap, lstm_2])
+    concatted = keras.layers.Concatenate()([cnn_flat, stft_flat, lstm_2])
     output_layer = keras.layers.Dense(num_classes, activation="softmax")(concatted)
 
     return keras.models.Model(inputs=[orig_input_layer, stft_input_layer], outputs=output_layer)
@@ -86,8 +88,8 @@ def get_training_val_data(stft_name: str):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, random_state = 42)
     x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size = 0.25, random_state = 21)
 
-    stft_train = np.load(stft_input_dir.joinpath('x_t'))
-    stft_val = np.load(stft_input_dir.joinpath('x_v'))
+    stft_train = np.load(stft_input_dir.joinpath('x_t'), allow_pickle=True)
+    stft_val = np.load(stft_input_dir.joinpath('x_v'), allow_pickle=True)
     
     return x_train, x_val, stft_train, stft_val, y_train, y_val
 
@@ -110,7 +112,7 @@ def train_model(x_train, x_val, stft_train, stft_val, y_train, y_val, override_l
     batch_size = 32
     callbacks = [keras.callbacks.ModelCheckpoint(log_dir.joinpath("best_model.h5"), save_best_only=True, monitor="val_loss"),
                  keras.callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=4, min_lr=0.00001),
-                 keras.callbacks.EarlyStopping(monitor="val_loss", patience=18, verbose=1),]
+                 keras.callbacks.EarlyStopping(monitor="val_loss", patience=15, verbose=1),]
 
     model.compile (optimizer="adam",loss="sparse_categorical_crossentropy",
                    metrics=['sparse_categorical_accuracy'])
@@ -118,7 +120,9 @@ def train_model(x_train, x_val, stft_train, stft_val, y_train, y_val, override_l
     history = model.fit (training_inputs, y_train, batch_size=batch_size, epochs=epochs,
                         callbacks=callbacks, validation_data=(validation_inputs, y_val), verbose=1,)
 
-    # loss, accuracy, f1_score, precision, recall = model.evaluate(x_train, y_train, verbose=0)
+    # eval_results = model.evaluate(validation_inputs, y_val)
+
+
     y_pred_probs = model.predict(validation_inputs)
     y_pred = np.argmax(y_pred_probs, axis=1)
     f1 = "%.3f" % f1_score(y_val, y_pred, average='macro')
@@ -185,8 +189,8 @@ def train_model(x_train, x_val, stft_train, stft_val, y_train, y_val, override_l
 if __name__ == '__main__':
     # stfts = [(128, 128), (128,16), (128, 64), (128, 8),(64, 50), (64, 32), (64, 8)]
     stfts = [(128, 128)]
-    model_name = '3_branches_1st_try'
+    model_name = '3_branches_2nd_try'
     for n_fft, win_len in stfts:
         stft_name = f'stft_{n_fft}_{win_len}'
         x_train, x_val, stft_train, stft_val, y_train, y_val = get_training_val_data(stft_name)
-        train_model(x_train, x_val, stft_train, stft_val, y_train, y_val)
+        train_model(x_train, x_val, stft_train, stft_val, y_train, y_val, override_logs=True)
